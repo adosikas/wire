da8b0fb20af3f827fee763147b7590afec711ef7
adosikas <adosikas@users.noreply.github.com>
2017-09-05 00:28:55 +0200

    fixup bitwise.lua doc-comments

--- lua/entities/gmod_wire_expression2/core/bitwise.lua
+++ lua/entities/gmod_wire_expression2/core/bitwise.lua
@@ -3,24 +3,31 @@
 
 __e2setcost(2)
 
+--- Performs binary AND
 e2function number bAnd(a, b)
 	return bit.band(a, b)
 end
+--- Performs binary OR
 e2function number bOr(a, b)
 	return bit.bor(a, b)
 end
+--- Performs binary XOR
 e2function number bXor(a, b)
 	return bit.bxor(a, b)
 end
+--- Shifts bits to the right
 e2function number bShr(a, b)
 	return bit.rshift(a, b)
 end
+--- Shifts bits to the left
 e2function number bShl(a, b)
 	return bit.lshift(a, b)
 end
+--- Performs binary NOT
 e2function number bNot(n)
 	return bit.bnot(n)
 end
+--- Performs binary NOT with a given mininum number of bots
 e2function number bNot(n,bits)
 	if bits >= 32 || bits < 1 then
 		return (-1)-n
